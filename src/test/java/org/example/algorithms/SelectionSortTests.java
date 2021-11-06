3a8a446765d66bb6725dcb509cd330d73b7270bb
Yuriy Savchenko <ysavchen@outlook.com>
2021-11-06 20:53:53 +0300

    Add SelectionSort

---
+++ src/test/java/org/example/algorithms/SelectionSortTests.java
@@ -0,0 +1,29 @@
+package org.example.algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SelectionSortTests {
+
+    @Test
+    void selectionSort() {
+        var immutable = List.of(9, 14, 2, 5);
+        var actual = SelectionSort.sort(immutable);
+        var expected = new ArrayList<>(immutable);
+        Collections.sort(expected);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void sortEmptyList() {
+        List<Integer> immutable = Collections.emptyList();
+        List<Integer> sorted = SelectionSort.sort(immutable);
+        assertTrue(sorted.isEmpty());
+    }
+}
