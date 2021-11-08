be5a9bc33aa4f63f0533b0064949b722b984226c
Yuriy Savchenko <ysavchen@outlook.com>
2021-11-08 11:16:30 +0300

    Add recursive sum, count, max

---
+++ src/test/java/org/example/algorithms/quicksort/RecursiveMaxTests.java
@@ -0,0 +1,22 @@
+package org.example.algorithms.quicksort;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RecursiveMaxTests {
+
+    @Test
+    void recursiveMax() {
+        int[] array = {1, 4, 3, 2};
+        int result = RecursiveMax.recursiveMax(array);
+        assertEquals(4, result);
+    }
+
+    @Test
+    void recursiveMaxEmptyArray() {
+        int[] array = {};
+        assertThrows(IllegalArgumentException.class, () -> RecursiveMax.recursiveMax(array));
+    }
+}
