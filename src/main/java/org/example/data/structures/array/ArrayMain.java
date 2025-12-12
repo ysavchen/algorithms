37df9fbbf849bac188892c999cec02cb83a74900
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-12 04:29:38 +0300

    Rename StringArray to UnsortedArray

--- src/main/java/org/example/data/structures/array/ArrayMain.java
+++ src/main/java/org/example/data/structures/array/ArrayMain.java
@@ -3,10 +3,10 @@ package org.example.data.structures.array;
 public class ArrayMain {
 
     public static void main(String[] args) {
-        var array = new StringArray(3);
+        var array = new UnsortedArray<String>(3);
         array.setElement(0, "Anna");
         array.setElement(1, "Maria");
         array.setElement(2, "Elena");
-        System.out.println(array);
+        System.out.println(array.getNumberOfElements());
     }
 }
--- src/main/java/org/example/data/structures/array/UnsortedArray.java
+++ src/main/java/org/example/data/structures/array/UnsortedArray.java
@@ -2,33 +2,47 @@ package org.example.data.structures.array;
 
 import java.util.Arrays;
 
-public class StringArray {
+// 1. Убрать индекс из setElement()
+// 2. Поддерживать выравнивание по левому краю
+public class UnsortedArray<T> {
 
-    private final String[] elementData;
+    private final T[] elementData;
 
-    public StringArray(int size) {
+    private int numberOfElements;
+
+    public UnsortedArray(int size) {
         if (size <= 0) {
             throw new IllegalArgumentException("Size must be greater than 0");
         }
-        this.elementData = new String[size];
+        this.elementData = (T[]) new Object[size];
     }
 
     public int getSize() {
         return elementData.length;
     }
 
-    public String getElement(int index) {
+    public int getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public T getElement(int index) {
         if (index < 0 || index >= elementData.length) {
             throw new ArrayIndexOutOfBoundsException();
         }
         return elementData[index];
     }
 
-    public void setElement(int index, String element) {
+    public void setElement(int index, T element) {
         if (index < 0 || index >= elementData.length) {
             throw new ArrayIndexOutOfBoundsException();
         }
         elementData[index] = element;
+        numberOfElements++;
+    }
+
+    public void removeElement(int index) {
+        elementData[index] = null;
+        numberOfElements--;
     }
 
     @Override
