f545eeb53d46a827f4fe3dfc813a863c0928cc9a
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-24 15:52:27 +0300

    Add coderun directory

---
+++ src/main/java/org/example/coderun/Main.java
@@ -0,0 +1,20 @@
+package org.example.coderun;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        /*
+        Пример ввода и вывода числа n, где -10^9 < n < 10^9:
+        int n = Integer.parseInt(reader.readLine());
+        writer.write(String.valueOf(n));
+        */
+
+        reader.close();
+        writer.close();
+    }
+}
