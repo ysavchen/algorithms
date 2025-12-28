25b5b88eb952fa3395090c84bfb8386df1a9e985
Yuriy Savchenko <ysavchen@outlook.com>
2025-12-28 09:38:47 +0300

    Rough dictionaries

---
+++ src/main/java/org/example/data/structures/dictionary/HashTable.java
@@ -0,0 +1,7 @@
+package org.example.data.structures.dictionary;
+
+/**
+ * Хеш-таблица на основе массива с разрешением конфликтов методом цепочек
+ */
+public class HashTable {
+}
---
+++ src/main/java/org/example/data/structures/queue/PriorityQueue.java
@@ -0,0 +1,7 @@
+package org.example.data.structures.queue;
+
+/**
+ * Очередь с приоритетом на основе двоичной кучи (binary heap), уложенной в статический массив
+ */
+public class PriorityQueue {
+}
