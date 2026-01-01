b11a6921cee10ef8db0547c1c948e37a86049974
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-01 20:15:24 +0300

    Update tests

--- src/test/java/org/example/data/structures/array/SortedArrayTests.java
+++ src/test/java/org/example/data/structures/array/SortedArrayTests.java
@@ -1,5 +1,7 @@
 package org.example.data.structures.array;
 
+import org.example.data.structures.array.exception.EmptyArrayException;
+import org.example.data.structures.array.exception.FullArrayException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,7 +22,7 @@ public class SortedArrayTests {
 
     @Test
     void getElement() {
-        var array = new UnsortedArray<String>(1);
+        var array = new SortedArray<String>(1);
         var element = "Anna";
         array.addElement(element);
         assertEquals(element, array.getElement(0));
@@ -28,39 +30,73 @@ public class SortedArrayTests {
 
     @Test
     void getNullElement() {
-        var array = new UnsortedArray<String>(1);
+        var array = new SortedArray<String>(1);
         assertNull(array.getElement(0));
     }
 
     @Test
     void getElementByNegativeIndex() {
-        var array = new UnsortedArray<String>(1);
+        var array = new SortedArray<String>(1);
         assertThrows(ArrayIndexOutOfBoundsException.class, () -> array.getElement(-1));
     }
 
     @Test
     void getElementOutOfBounds() {
-        var array = new UnsortedArray<String>(1);
+        var array = new SortedArray<String>(1);
         assertThrows(ArrayIndexOutOfBoundsException.class, () -> array.getElement(1));
     }
 
     @Test
     void findElement() {
-        var array = new UnsortedArray<String>(1);
+        var array = new SortedArray<String>(1);
         array.addElement("Anna");
         assertEquals(0, array.findElement("Anna"));
     }
 
     @Test
     void findNullElement() {
-        var array = new UnsortedArray<String>(2);
+        var array = new SortedArray<String>(2);
         array.addElement("Anna");
         assertEquals(1, array.findElement(null));
     }
 
     @Test
     void findNonExistingElement() {
-        var array = new UnsortedArray<String>(1);
+        var array = new SortedArray<String>(1);
         assertEquals(-1, array.findElement("Anna"));
+    }
+
+    @Test
+    void addElement() {
+        var array = new SortedArray<String>(1);
+        array.addElement("Anna");
+        assertEquals(1, array.getSize());
+        assertEquals(1, array.getNumberOfElements());
+    }
+
+    @Test
+    void addElementToFullArray() {
+        var array = new SortedArray<String>(1);
+        array.addElement("Anna");
+        assertThrows(FullArrayException.class, () -> array.addElement("Maria"));
+    }
+
+    @Test
+    void removeElement() {
+        var array = new SortedArray<String>(1);
+        var element = "Anna";
+        array.addElement(element);
+        assertEquals(1, array.getSize());
+        assertEquals(1, array.getNumberOfElements());
+
+        array.removeElement(element);
+        assertEquals(1, array.getSize());
+        assertEquals(0, array.getNumberOfElements());
+    }
+
+    @Test
+    void removeElementFromEmptyArray() {
+        var array = new SortedArray<String>(5);
+        assertThrows(EmptyArrayException.class, () -> array.removeElement("Anna"));
     }
 }
