7bde15f9ed2ae44636293d11c2b60e5e1ee9db78
Yuriy Savchenko <ysavchen@outlook.com>
2026-01-01 20:11:52 +0300

    Update tests

--- src/test/java/org/example/data/structures/array/SortedArrayTests.java
+++ src/test/java/org/example/data/structures/array/SortedArrayTests.java
@@ -2,8 +2,7 @@ package org.example.data.structures.array;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SortedArrayTests {
 
@@ -17,5 +16,51 @@ public class SortedArrayTests {
     @Test
     void createArrayWithNegativeSize() {
         assertThrows(IllegalArgumentException.class, () -> new SortedArray<String>(-1));
+    }
+
+    @Test
+    void getElement() {
+        var array = new UnsortedArray<String>(1);
+        var element = "Anna";
+        array.addElement(element);
+        assertEquals(element, array.getElement(0));
+    }
+
+    @Test
+    void getNullElement() {
+        var array = new UnsortedArray<String>(1);
+        assertNull(array.getElement(0));
+    }
+
+    @Test
+    void getElementByNegativeIndex() {
+        var array = new UnsortedArray<String>(1);
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> array.getElement(-1));
+    }
+
+    @Test
+    void getElementOutOfBounds() {
+        var array = new UnsortedArray<String>(1);
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> array.getElement(1));
+    }
+
+    @Test
+    void findElement() {
+        var array = new UnsortedArray<String>(1);
+        array.addElement("Anna");
+        assertEquals(0, array.findElement("Anna"));
+    }
+
+    @Test
+    void findNullElement() {
+        var array = new UnsortedArray<String>(2);
+        array.addElement("Anna");
+        assertEquals(1, array.findElement(null));
+    }
+
+    @Test
+    void findNonExistingElement() {
+        var array = new UnsortedArray<String>(1);
+        assertEquals(-1, array.findElement("Anna"));
     }
 }
