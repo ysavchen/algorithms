1e301a25b0efb4e3983dc93733b6dc2f98baccaf
Yuriy Savchenko <ysavchen@outlook.com>
2026-05-19 14:20:19 +0300

    Update BalancedSearchTree

--- src/main/java/org/example/data/structures/tree/BalancedSearchTree.java
+++ src/main/java/org/example/data/structures/tree/BalancedSearchTree.java
@@ -4,4 +4,17 @@ package org.example.data.structures.tree;
  * Сбалансированное дерево поиска (АВЛ-дерево)
  */
 public class BalancedSearchTree {
+
+    private static class Node {
+
+        private final int value;
+        private int height = 1;
+
+        private Node left = null;
+        private Node right = null;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
 }
